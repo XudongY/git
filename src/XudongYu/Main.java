6c0da9ac10f2cc9be2fbf3198b0f43a62b1c079f
XudongY <31521395+XudongY@users.noreply.github.com>
2017-09-21 23:27:17 -0400

    first commit

---
+++ src/XudongYu/Main.java
@@ -0,0 +1,10 @@
+package XudongYu;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code heregit
+        System.out.println("hello word");
+        System.out.println("exchange");
+    }
+}
